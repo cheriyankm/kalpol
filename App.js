b5b073cc0de6d1996c8ba4dfb2b99cd918caf6eb
cheriyankmaman <cheriyankmaman@gmail.com>
2022-03-25 09:57:52 +0000

    first commit

--- App.js
+++ App.js
@@ -1,21 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import { NativeBaseProvider, Box, Center, Heading, Text, Container, ScrollView, HStack, Flex, Divider, Button, Input, Stack, Image, VStack } from "native-base";
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 export default function App() {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeBaseProvider>
+      <Stack space={3} alignItems="center" m="3%" maxW="100%">
+        <HStack space={3} alignItems="center" maxW="100%">
+          <VStack space={8} maxW="100%">
+            <Input variant="filled" placeholder="Your Name" />
+            <Input variant="filled" placeholder="Room name(Optional)" />
+            <Button onPress={() => console.log("Create room")}>Create room</Button>
+          </VStack>
+          <Divider my={2} orientation="vertical" />
+          <VStack space={8} maxW="100%">
+            <Input variant="filled" placeholder="Your Name" />
+            <Input variant="filled" placeholder="Room id" />
+            <Button onPress={() => console.log(name)}>Join room</Button>
+          </VStack>
+        </HStack>
+      </Stack>
+    </NativeBaseProvider>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ pages/waiting/Waiting.js
@@ -0,0 +1,10 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { NativeBaseProvider, Box, Center, Heading, Text, Container, ScrollView, HStack, Flex, Divider, Button, Input, Stack, Image, VStack } from "native-base";
+
+export default function Waiting() {
+  return (
+      <Text>Hello</Text>
+  );
+}
